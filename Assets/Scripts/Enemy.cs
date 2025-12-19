1866f7ca3886b056e30e9234b2e55288b277cec2
ThatRahimGuy <rahim.ahmed22nd@gmail.com>
2025-12-19 05:14:43 +0000

    added in the proximity distance thingy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -47,7 +47,10 @@ public class Enemy : MonoBehaviour, IDamageable
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
-
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        
+        if(distance < 5)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
     }
 }
