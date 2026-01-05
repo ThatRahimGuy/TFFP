8eb916a51ebc774893f9e3050e2e7e105de144b5
ThatRahimGuy <rahim.ahmed22nd@gmail.com>
2026-01-05 01:46:07 +0000

    main menu stuff
    tried to add in a sfx & music manager(failed) tried to make a audio settings(also failed)

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,9 +1,24 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour
 {
 
+    public AudioMixer audioMixer;
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    private void Start()
+    {
+        LoadVolume();
+        MusicManager.Instance.PlayMusic("MainMenuMusic");
+    }
+    public void Play()
+    {
+        MusicManager.Instance.PlayMusic("MainMenuMusic");
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,4 +29,31 @@ public class Buttons : MonoBehaviour
         Debug.Log("Quit!");
         Application.Quit(); 
     }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("Music Volume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+    }
+
 }
+
